d29d240ff89fa0af8cfeed305ce7b6838598c850
praveen-2311 <praveencse112@gmail.com>
2026-03-05 22:21:33 +0530

    Add files via upload

---
+++ pat6.java
@@ -0,0 +1,16 @@
+
+public class pat6 {
+ public static void main(String[] args)
+ {
+    int n = 4;
+    for(int i=n;i>0;i--)
+            {
+                for(int j=1;j<=i;j++)
+                {
+                    System.out.print("*");
+                }
+                System.out.println("");
+            }
+ }
+    
+}
