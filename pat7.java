95329c9d93b692f325a72a8a36fd1607b954793f
praveen-2311 <praveencse112@gmail.com>
2026-03-05 22:35:43 +0530

    Add files via upload

---
+++ pat7.java
@@ -0,0 +1,13 @@
+public class pat7 {
+    public static void main(String[] args) {
+        int n = 4;
+       for(int i=n;i>0;i--)
+       {
+           for(int j=i;j>0;j--)
+           {
+               System.out.print(j);
+           }
+           System.out.println("");
+       }
+    }
+}
