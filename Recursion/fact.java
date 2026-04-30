27cb4be0efdc2844061be44d8deec7749d637819
praveen-2311 <praveencse112@gmail.com>
2026-04-30 22:41:12 +0530

    Create fact.java

---
+++ Recursion/fact.java
@@ -0,0 +1,15 @@
+class Main {
+    int func(int n)
+    {
+        if(n == 0)
+        {
+            return 1;
+        }
+        return n * func(n - 1);
+    }
+    
+    public static void main(String[] args) {
+        Main m = new Main();
+        System.out.println(m.func(5));
+    }
+}
